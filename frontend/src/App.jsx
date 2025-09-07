7ba07dd0578dfa359f9ab5a02f3255e75572daba
Shubham Tripathi <shubhamtripathi1042@gmail.com>
2025-09-07 07:23:04 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react' 
-import { Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Contact from './pages/Contact';
 import About from './pages/About';
